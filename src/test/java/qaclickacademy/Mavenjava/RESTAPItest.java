ae24a74af59a7c26a2fddbff42c48a192b9acdce
jiajing <jj750913@dal.ca>
2022-07-25 16:11:38 -0300

    my first commit

---
+++ src/test/java/qaclickacademy/Mavenjava/RESTAPItest.java
@@ -0,0 +1,22 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RESTAPItest {
+
+	
+	
+	@Test
+	public void posJira()
+	{
+		System.out.println("posJira");
+	}
+
+		
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+	}
+	
+}
